d35c60a54c19ad8c6ecb4933613dd924dd088cce
Xu Wang <xuwang@gmail.com>
2015-05-26 17:43:41 -0700

    imporve security: encode username in auth token

--- ngClient/js/factory.js
+++ ngClient/js/factory.js
@@ -1,13 +1,11 @@
-
 myApp.factory('dataFactory', function($http) {
-  /** https://docs.angularjs.org/guide/providers **/
-  var urlBase = 'http://localhost:3000/api/v1/products';
-  var _prodFactory = {};
+	/** https://docs.angularjs.org/guide/providers **/
+	var urlBase = 'https://localhost:3000/api/v1/products';
+	var _prodFactory = {};
 
-  _prodFactory.getProducts = function() {
-    return $http.get(urlBase);
-  }; 
+	_prodFactory.getProducts = function() {
+		return $http.get(urlBase);
+	};
 
-  return _prodFactory;
+	return _prodFactory;
 });
-
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var https = require('https');
 
 var app = express();
 
@@ -10,16 +9,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 
 app.all('/*', function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
+	// CORS headers
+	res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	// Set custom headers for CORS
+	res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+	if (req.method == 'OPTIONS') {
+		res.status(200).end();
+	} else {
+		next();
+	}
 });
 
 // Auth Middleware - This will check if the token is valid
@@ -32,23 +31,34 @@ app.use('/', require('./routes'));
 
 // If no route is matched by now, it must be a 404
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // Start the server
 app.set('port', process.env.PORT || 3000);
 
+/*
+var server = app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
+
+*/
 
 // start https server
+
+var https = require('https');
+var fs = require('fs');
+
+var hskey = fs.readFileSync('./ssl/hacksparrow-key.pem');
+var hscert = fs.readFileSync('./ssl/hacksparrow-cert.pem')
+
+var https_options = {
+    key: hskey,
+    cert: hscert
+};
 server = https.createServer(https_options, app).listen(app.get('port'), function() {
   console.log('Express https server listening on port ' + server.address().port);
 });
 
-/*
-// Start http server
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
-*/
